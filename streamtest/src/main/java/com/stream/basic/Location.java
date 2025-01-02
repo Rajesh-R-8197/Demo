35fa9fe9fed47065742dfe2e8d4cfdcdbe66c7b5
Rajesh-R-8197 <rajeshraju8197@gmail.com>
2025-01-02 07:43:20 +0530

    add files

---
+++ streamtest/src/main/java/com/stream/basic/Location.java
@@ -0,0 +1,4 @@
+package com.stream.basic;
+
+public record Location(String id) {
+}
--- streamtest/src/main/java/com/stream/basic/TestStream.java
+++ streamtest/src/main/java/com/stream/basic/TestStream.java
@@ -1,4 +1,28 @@
 package com.stream.basic;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class TestStream {
+
+    public static void main(String[] args) {
+        System.out.println("Starting test Stream apis");
+
+        /**
+         *filter,consumer,supplier and predicate
+         * */
+
+        List<Integer> x = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> y = x.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(y);
+
+
+
+    }
+
+
 }
